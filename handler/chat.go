c851afabecea657c5a8880ea1ce832aecf1f8c97
Cesar <c.caycho46@gmail.com>
2021-10-01 23:03:19 -0500

    asignChatvariables

--- handler/chat.go
+++ handler/chat.go
@@ -1,10 +1,14 @@
 package handler
 
 import (
+	"crypto/sha256"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
+	"github.com/cfabrica46/chat-gin-web-socket/token"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
@@ -16,9 +20,10 @@ type myConn struct {
 }
 
 type message struct {
-	Token          string `json:"token"`
-	Message        string `json:"message"`
-	UsersConnected string `json:"usersConnected"`
+	Token           string   `json:"token"`
+	Message         string   `json:"message"`
+	UsersConnected  []string `json:"usersConnected"`
+	IsStatusMessage bool     `json:"isStatusMessage"`
 }
 
 var upgrader = websocket.Upgrader{}
@@ -26,7 +31,6 @@ var upgrader = websocket.Upgrader{}
 var rooms = make(map[string]map[string]myConn)
 
 func Chat(c *gin.Context) {
-	var owner string
 	var idRoom, myID string
 	var myToken string
 	// var ocult bool
@@ -55,56 +59,49 @@ func Chat(c *gin.Context) {
 			delete(rooms[idRoom], myID)
 
 			users := getUsersIntoRoom(rooms[idRoom])
-			m := message{Token: myToken, ""}
+			m := message{Token: myToken, Message: "has gone out to the chat", UsersConnected: users, IsStatusMessage: true}
 
-			data, err := json.Marshal(m)
+			msgJSON, err := json.Marshal(m)
 			if err != nil {
-				data = []byte("")
+				msgJSON = []byte{}
 			}
 
 			for i := range rooms[idRoom] {
-				go sendMessage(rooms[idRoom][i].Conn, data)
+				go sendMessage(rooms[idRoom][i].Conn, msgJSON)
 			}
 			return
 		}
 
-		/* if msg.ByServer && msg.Data == "has gone out to the chat" {
-			delete(rooms[idRoom], myID)
-			return
-		}
-
-		if msg.ByServer && strings.Contains(msg.Data, "idRoom:") {
-			h := sha256.Sum256([]byte(msg.Data))
-			idRoom = fmt.Sprintf("%x\n", h)
-
-			owner = msg.Owner
-
-			if len(rooms[idRoom]) == 0 {
-				rooms[idRoom] = make(map[string]myConn)
-				rooms[idRoom][myID] = myConn{Conn: conn, Owner: msg.Owner}
-			} else {
-				rooms[idRoom][myID] = myConn{Conn: conn, Owner: msg.Owner}
+		if msg.IsStatusMessage && strings.Contains(msg.Message, "idRoom:") && myToken == "" && idRoom == "" {
+			idRoom, myToken, err = asignChatVariables(conn, msg, myID)
+			if err != nil {
+				return
 			}
 			// ocult = true
 		}
 
-		users := getUsers(rooms[idRoom])
+		if msg.IsStatusMessage && msg.Message == "has gone out to the chat" {
+			delete(rooms[idRoom], myID)
+			return
+		}
 
-		msg.Users = users
+		// users := getUsers(rooms[idRoom])
 
-		data, err := json.Marshal(msg)
+		// msg.Users = users
+
+		/* data, err := json.Marshal(msg)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"ErrMessage": "Internal Error",
 			})
 			return
-		}
+		} */
 
 		// if !ocult {
-		for i := range rooms[idRoom] {
+		/* for i := range rooms[idRoom] {
 			go sendMessage(rooms[idRoom][i].Conn, data)
-		}
-		// } */
+		} */
+		// }
 
 	}
 }
@@ -132,8 +129,33 @@ func getUsersIntoRoom(room map[string]myConn) (users []string) {
 	return
 }
 
+func asignChatVariables(conn *websocket.Conn, msg message, myID string) (idRoom, myToken string, err error) {
+	hash := sha256.Sum256([]byte(msg.Message))
+	idRoom = fmt.Sprintf("%x\n", hash)
+
+	if rooms[idRoom] != nil {
+		return
+	}
+
+	myToken = msg.Token
+
+	tokenStructure, err := token.ExtractTokenStructFromClaims(myToken)
+	if err != nil {
+		return
+	}
+
+	if len(rooms[idRoom]) == 0 {
+		rooms[idRoom] = make(map[string]myConn)
+		rooms[idRoom][myID] = myConn{Conn: conn, Owner: tokenStructure.Username}
+	} else {
+		rooms[idRoom][myID] = myConn{Conn: conn, Owner: tokenStructure.Username}
+	}
+
+	return
+}
+
 func ping(conn *websocket.Conn) {
-	var msg = message{Data: "ping", ByServer: true}
+	var msg = message{Message: "ping", IsStatusMessage: true}
 	dataJSON, err := json.Marshal(msg)
 	if err != nil {
 		return
