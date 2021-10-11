26787810ad2f159cf5b8c6ef261770adb06136e4
Cesar <c.caycho46@gmail.com>
2021-10-10 20:42:29 -0500

    completed issues

--- files/src/jsx/form_chat.jsx
+++ files/src/jsx/form_chat.jsx
@@ -31,7 +31,7 @@ function DisplayUsers(props) {
     return (
         <div ref={props.ref} className="chat-users">
             <i onClick={() => props.onClickOcult()} class="fas fa-times"></i>
-            <p className="chat-users--title">Users Connected</p>
+            <p className="chat-users--title">Users Connected:</p>
             <ul className="chat-users--list">
                 {props.elements.map((e) => (
                     <li className="chat-users--user"> {e}</li>
